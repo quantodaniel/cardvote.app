85784fcd74bc2a5943faf322ffe348ebddb2507a
Daniel Almeida <quantodaniel@gmail.com>
2023-05-25 20:00:09 -0300

    Added openai to generate random avatars

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { useAuth } from "@/hooks/useAuth";
 import Image from "next/image";
 import Link from "next/link";
 
 export const Navbar = () => {
+  const { currentUser } = useAuth();
+  const currentAvatar = currentUser?.photoURL || "/no-avatar.png";
+
   return (
     <nav className="my-8 flex gap-4">
       <Link href="/" className="btn btn-ghost h-auto hover:bg-transparent p-0">
@@ -17,6 +23,19 @@ export const Navbar = () => {
       <div>
         <h1 className="text-4xl">cardvote.app</h1>
         <h2 className="text-xl">Planning to the Next Level!</h2>
+      </div>
+
+      <div className="flex flex-1 justify-end items-center">
+        {currentUser?.displayName}
+        <div className="avatar online">
+          <Image
+            src={currentAvatar}
+            className="rounded-full shadow-lg"
+            width={36}
+            height={36}
+            alt="Logged User Avatar"
+          />
+        </div>
       </div>
     </nav>
   );
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -7,18 +7,46 @@ import {
   getAuth,
   onAuthStateChanged,
   signInAnonymously,
+  updateProfile,
 } from "firebase/auth";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+const updateUserAvatar = async () => {
+  const auth = getAuth();
+  if (!auth.currentUser) return;
+
+  const currentAvatar = localStorage.getItem("avatar");
+  if (currentAvatar) return;
+
+  const randomUserResponse = await fetch("/api/avatar");
+  const randomUser = await randomUserResponse.json();
+  if (!randomUser.photoURL) return;
+
+  localStorage.setItem("avatar", randomUser.photoURL);
+
+  updateProfile(auth.currentUser, {
+    photoURL: randomUser.photoURL,
+    displayName: randomUser.displayName,
+  });
+};
 
 export const useAuth = () => {
   const [user, setUser] = useState<User | null>(null);
+  const userUpdated = useRef(false);
 
   useEffect(() => {
     const auth = getAuth();
 
-    onAuthStateChanged(auth, (user) => {
-      if (user) return setUser(user);
+    onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+
+        if (!userUpdated.current) {
+          updateUserAvatar();
+          userUpdated.current = true;
+        }
+      }
     });
 
     if (!auth.currentUser) signInAnonymously(auth);
